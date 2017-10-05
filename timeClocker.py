17a6e683ad819896dc647a9babb98b4b7735efba
auraten <auraten@gmail.com>
2017-10-05 16:01:12 -0500

    TimeClocker 2.0
    TimeClocker adapted to UltiPro

--- timeClocker.py
+++ timeClocker.py
@@ -1,4 +1,4 @@
-#timeClocker 1.2.3 - Forms, Days, Lunches, Choice of Start Times
+#timeClocker 2.0 - Forms, Days, Lunches, Choice of Start Times
 
 import re
 from selenium import webdriver
@@ -17,23 +17,23 @@ chrome_options.add_experimental_option('prefs', {
     }
 })
 chrome_options.add_argument("window-position=1000,0")
-chrome_options.add_argument("window-size=900,900")
+chrome_options.add_argument("window-size=900,1800")
 browser = webdriver.Chrome('C:\Program Files\Time Clocker\chromedriver.exe',chrome_options=chrome_options)
 
 def getCreds():
     #open the credentials form
     browser.get('C:\\Program Files\\Time Clocker\\form.html')
-
+    
     #get the current URL and wait until it has a "?" in it,
     #which indicates the user has pressed submit
     currentUrl = str(browser.current_url)
     while "?" not in currentUrl:
         time.sleep(1)
         currentUrl = str(browser.current_url)
-
+    
     #once the user has clicked submit, parse the URL and grab the get values
     parsedUrl = urlparse(currentUrl)
-    userDataDict = parse_qs(parsedUrl.query) 
+    userDataDict = parse_qs(parsedUrl.query)
     
     #strip the unnecessary characters from the dict values,
     #make it a list
@@ -41,13 +41,13 @@ def getCreds():
     for value in userDataDict.values():
         value = str(value)[2:-2]
         credsList.append(value)
-
+    
     #ensure nothing is blank
     listCount = len(credsList)
     if listCount < 8:
         print("Please fill in all fields!")
         return getCreds()
-
+    
     #ensure dayin precedes dayout
     if credsList[2] > credsList[3]:
         print("Please select days in the proper order!")
@@ -65,119 +65,227 @@ def getCreds():
     if len(timeList) % 2 == 1:
         print("Please ensure you clock properly!")
         return getCreds()
-
+    
     #ensure that times don't conflict and are actually clockable
     for item in range(0,len(timeList)-1):
         if timeList[item+1] <= timeList[item]:
             print("Times are unclockable! Please ensure your entered times follow each other.")
             return getCreds()
-
+    
     #make it a tuple containing name, password, dayin, dayout, time in,
     #lunch start, lunch end, time out
     dataTup = tuple(credsList)
     return dataTup
-
+    
 def login(credsTup):
     #navigates to login page
-    browser.get('https://www.exponenthr.com/service/EmpLogon.asp')
+    browser.get('https://nw12.ultipro.com/login.aspx')
     #identify login fields and fill with creds
-    emailElem = browser.find_element_by_id('USERID')
+    emailElem = browser.find_element_by_id('ctl00_Content_Login1_UserName')
     emailElem.send_keys(credsTup[0])
-    passwordElem = browser.find_element_by_id('PW')
+    passwordElem = browser.find_element_by_id('ctl00_Content_Login1_Password')
     passwordElem.send_keys(credsTup[1])
 
     #submit login
-    submitButton = browser.find_element_by_name('B1')
+    submitButton = browser.find_element_by_id('ctl00_Content_Login1_LoginButton')
     submitButton.click()
-
-    #check we logged in successfully
-    pageTitle = str(browser.title)
-
+    
     #check that we got the pw right, if not restart
-    pageTitle = str(browser.title)
-    if pageTitle == 'Validate Failed':
+    currentUrl = str(browser.current_url)
+    if currentUrl == 'https://nw12.ultipro.com/login.aspx':
         print('Login incorrect, try again!')
         return login(getCreds())
     else:
         print("Login Successful!\n")
-
+    
 def main():
     #make dataTup a global var
     dataTup = getCreds()
     login(dataTup)
     
-    #prompt the user to enter their sec answer then
-    #wait til they get to the home page
-    print("Please answer your security question in the browser.\n")
-    currentUrl = str(browser.current_url)
-    while "https://www.exponenthr.com/service/EmpWelcome.asp" not in currentUrl:
-        time.sleep(1)
-        currentUrl = str(browser.current_url)
-
     #navigate to timeclock page
-    browser.get('https://www.exponenthr.com/service/EmpTimeClock.asp');
+    menuButton = browser.find_element_by_xpath('//*[@id=\"menuButtonContainer\"]/div[1]')
+    menuButton.click()
+    myselfButton = browser.find_element_by_xpath('//*[@id=\"megaMenuHeader\"]/div[2]/div[2]')
+    myselfButton.click()
+    timeMGMT = browser.find_element_by_xpath('//*[@id=\"newMegaMenuContainer\"]/div[2]/div[3]/div[3]/div[1]/h3')
+    timeMGMT.click()
 
-    #get the text of the element containing already entered holiday dates
-    holiday = browser.find_element_by_id('TableScript1')
-    holiText = holiday.text
+    #drop down into the proper frame
+    frame = browser.find_element_by_id('ContentFrame')
+    browser.switch_to.frame(frame)
+    frame = browser.find_element_by_id('ctl00_Content_UTMFrame')
+    browser.switch_to.frame(frame)
+    frame = browser.find_element_by_id('MainContentFrame')
+    browser.switch_to.frame(frame)
+    frame = browser.find_element_by_id('main_frame')
+    browser.switch_to.frame(frame)
 
-    #use regex to search the text string for all clocked dates
-    #in the xx/xx/xxxx format
-    dateMatch = re.findall(r"(\d+/\d+/\d+)",holiText)
+    #grab the string that specifies the date range
+    dateString = browser.find_element_by_class_name('timesheetHeaderText')
+    dateText = dateString.text
 
-    #get a list of values in the date dropdown to compare against
-    dateDropDown = Select(browser.find_element_by_name('ThePayDateINOUTTwo'))
-    thisWeekOptions = dateDropDown.options
-    valuesList = []
-    for option in thisWeekOptions:
-        valuesList.append(option.get_attribute("value"))
+    #split it
+    dates = dateText.split(' to ')
 
-    #create the list of date values for informational use later
+    #convert it to datetime objects, do math to determine if 2 or 3 week period
+    startStr = dates[0]
+    endStr = dates[1]
+    start = datetime.datetime.strptime(startStr, 'My Timesheet for %B %d, %Y')
+    end = datetime.datetime.strptime(endStr, '%B %d, %Y')
+    w = end - start
+    if w.days < 14:
+        weeks = 2
+    else:
+        weeks = 3
+
+    #loop to ensure enough rows exist
+    for _ in range(w.days):
+        #click the add button
+        addButton = browser.find_element_by_id('ImgAddRow')
+        addButton.click()
+
+    #click the date field
+    dateField = browser.find_element_by_id('gdvTS_rw_' + str(w.days) + '_cl_1')
+    dateField.click()
+
+    #get a list of ALL values in the date dropdown to compare against
+    dateDropDown = Select(browser.find_element_by_id('gdvTS_rw_' + str(w.days) + '_TPDATE_slc'))
+    periodDates = dateDropDown.options
+
+    #convert to text list
     textList = []
-    for option in thisWeekOptions:
+    for option in periodDates:
         textList.append(option.text)
 
-    #create a list of matching items between the list of dates clocked,
-    #and dates in the dropdown
-    matchSet = list(set(dateMatch).intersection(valuesList))
+    #counter for rows
+    z = 0
+    
+    #fetch list for already clocked dates
+    clockedDates = []
+    rowStr = 'gdvTS_rw_' + str(z) + '_cl_1'
+    dateField = browser.find_element_by_id(rowStr)
+    dateText = dateField.text
 
-    #find the all indexes for the matching items and sort
-    dateIndexes = []
-    for item in matchSet:
-        dateIndexes.append(valuesList.index(item))
-    dateIndexes.sort(key=int)
+    #loop through filled rows and append those dates to a list
+    while dateText != '':
+        clockedDates.append(dateText)
+        z = z + 1
+        rowStr = 'gdvTS_rw_' + str(z) + '_cl_1'
+        dateField = browser.find_element_by_id(rowStr)
+        dateText = dateField.text
 
-    #get how many items are in the date drowdown and div by 7 to get # of weeks
-    weeks = (len(dateDropDown.options)/7)
+    #instantiate list of valid dates
+    validDates = []
+
+    #counter for weeks
     x = 0
-
+    
+    #run through the users chosen dates to compare clocked dates against the list of all dates to create a list of valid dates
     while x < weeks:
         y = x * 7
-        for i in range(y+int(dataTup[2]),y+int(dataTup[3])+1):        
-            #jump to the day we are on and print it out
-            dateDropDown = Select(browser.find_element_by_name('ThePayDateINOUTTwo'))
-            dateDropDown.select_by_index(i)
-            print(textList[i])
-
-            #continue if we have a matching date index,
-            #effectively skipping a day
-            if i in dateIndexes:
-                print("Already Clocked or PTO/Holiday, Skipping!\n")
-                continue
-            
-            #loop through the times in the tuple and clock them
-            for tupID in range(4,8):
-                #skip if blank (for no lunch
-                if dataTup[tupID] == 'NA':
-                    continue
-                else:
-                    #jump to in time dropdown & submit
-                    timeDropDown = Select(browser.find_element_by_name('ThePayTimeINOUT'))
-                    timeDropDown.select_by_value(dataTup[tupID])
-                    punchButton = browser.find_element_by_name('B1')
-                    punchButton.click()
-            
-            print("Clocked!\n")
         x = x + 1
+        for i in range(y+int(dataTup[2])+1,y+int(dataTup[3])+2):
+            if textList[i] not in clockedDates:
+                validDates.append(textList[i])
+
+    #find the index numbers of those valid dates in the list of ALL dates
+    indexList = []
+    for i in validDates:
+        indexList.append(textList.index(i))
+        
+    #clock based on only the valid dates
+    #z counter is already iterated properly to avoid row collisions
+    for i in indexList:   
+        #print the date we're working on
+        print(textList[i])
+
+        #insert the row counter into the string to interate rows
+        rowStr = 'gdvTS_rw_' + str(z) + '_cl_1'
+        dateField = browser.find_element_by_id(rowStr)
+        
+        #click the date field
+        dateField.click()
+
+        #insert the counter into the string to iterate dates in the drop down
+        dropDownStr = 'gdvTS_rw_' + str(z) + '_TPDATE_slc'
+        dateDropDown = Select(browser.find_element_by_id(dropDownStr))
+        dateDropDown.select_by_index(i)
+
+        #open the pay code and select work
+        payStr = 'gdvTS_rw_' + str(z) + '_cl_2'
+        payField = browser.find_element_by_id(payStr)
+        payField.click()
+        payDropDownStr = 'gdvTS_rw_' + str(z) + '_NPAYCODE_slc'
+        payDropDown = Select(browser.find_element_by_id(payDropDownStr))
+        payDropDown.select_by_index(1)
+
+        #insert the times
+        timeStr = 'gdvTS_rw_' + str(z) + '_cl_3'
+        timeBox = browser.find_element_by_id(timeStr)
+        timeBox.click()
+        timeFieldStr = 'gdvTS_rw_' + str(z) + '_TIN_txt'
+        timeField = browser.find_element_by_id(timeFieldStr)
+        timeField.send_keys(dataTup[4])
+        
+        if dataTup[5] != 'NA':
+            #insert the lunch start time
+            timeStr = 'gdvTS_rw_' + str(z) + '_cl_4'
+            timeBox = browser.find_element_by_id(timeStr)
+            timeBox.click()
+            timeFieldStr = 'gdvTS_rw_' + str(z) + '_TOUT_txt'
+            timeField = browser.find_element_by_id(timeFieldStr)
+            timeField.send_keys(dataTup[5])
+
+            #iterate the row counter
+            z = z + 1
+
+            #click the add button
+            addButton = browser.find_element_by_id('ImgAddRow')
+            addButton.click()
+
+            #insert the row counter into the string to interate rows
+            rowStr = 'gdvTS_rw_' + str(z) + '_cl_1'
+            
+            #click the date field
+            dateField = browser.find_element_by_id(rowStr)
+            dateField.click()
+
+            #insert the counter into the string to iterate dates in the drop down
+            dropDownStr = 'gdvTS_rw_' + str(z) + '_TPDATE_slc'
+            dateDropDown = Select(browser.find_element_by_id(dropDownStr))
+            dateDropDown.select_by_index(i)
+
+            #open the pay code and select work
+            payStr = 'gdvTS_rw_' + str(z) + '_cl_2'
+            payField = browser.find_element_by_id(payStr)
+            payField.click()
+            payDropDownStr = 'gdvTS_rw_' + str(z) + '_NPAYCODE_slc'
+            payDropDown = Select(browser.find_element_by_id(payDropDownStr))
+            payDropDown.select_by_index(1)
+
+            #insert the resume work time
+            timeStr = 'gdvTS_rw_' + str(z) + '_cl_3'
+            timeBox = browser.find_element_by_id(timeStr)
+            timeBox.click()
+            timeFieldStr = 'gdvTS_rw_' + str(z) + '_TIN_txt'
+            timeField = browser.find_element_by_id(timeFieldStr)
+            timeField.send_keys(dataTup[6])
+
+        #insert the end of day time
+        timeStr = 'gdvTS_rw_' + str(z) + '_cl_4'
+        timeBox = browser.find_element_by_id(timeStr)
+        timeBox.click()
+        timeFieldStr = 'gdvTS_rw_' + str(z) + '_TOUT_txt'
+        timeField = browser.find_element_by_id(timeFieldStr)
+        timeField.send_keys(dataTup[7])
+
+        print('CLOCKED!\n')
+
+        #iterate the row counter
+        z = z + 1
+
+    submitButton = browser.find_element_by_id('btn_Apply')
+    submitButton.click()
 
     print("DONE! You may now close this window.")
